2f0a6db961e04d4374f4cb44db4f9e2bab544c57
Sourav-1026 <faruqueomar740@gmail.com>
2026-05-01 19:36:41 +0600

    Featured tiles part complete

--- src/components/FeaturesTiles.jsx
+++ src/components/FeaturesTiles.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import TilesCard from "./TilesCard";
 
 const getTiles = async () => {
   const res = await fetch("https://tiles-gallery-iota.vercel.app/data.json");
@@ -13,20 +14,9 @@ const FeaturesTiles = async () => {
   return (
     <div className="container mx-auto my-10">
       <h1 className="text-3xl font-bold text-center">Featured Tiles</h1>
-      <div className="grid grid-cols-3 gap-5 mt-3.5">
-        {tiles.map((t) => (
-          <div key={t.id} className="card bg-base-100  shadow-sm">
-            <figure>
-              <Image src={t.image} alt={t.title} width={300} height={300} />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">{t.tile}</h2>
-              <p>{t.description}</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">View Details</button>
-              </div>
-            </div>
-          </div>
+      <div className="grid grid-cols-4 gap-5 mt-3.5">
+        {tiles.slice(0, 4).map((t) => (
+          <TilesCard key={t.id} t={t} />
         ))}
       </div>
     </div>
---
+++ src/components/TilesCard.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import React from "react";
+
+const TilesCard = ({ t }) => {
+  return (
+    <div key={t.id} className="card bg-base-100 shadow-sm  border border-gray-300">
+      <div className="relative w-full aspect-square">
+        <Image src={t.image} alt={t.title} fill className="object-cover rounded-xl p-3" />
+      </div>
+      <div className="card-body">
+        <h2 className="card-title">{t.title}</h2>
+        <p>{t.description}</p>
+        <div className="card-actions">
+          <button className="btn w-full border border-[#1D9E75] bg-transparent text-[#1D9E75]">View Details</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TilesCard;
