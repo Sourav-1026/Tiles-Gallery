f530fcc83f40da171bf35a49b12fff0d0de22ca8
Sourav-1026 <faruqueomar740@gmail.com>
2026-05-01 16:19:40 +0600

    updated data json file

--- src/components/FeaturesTiles.jsx
+++ src/components/FeaturesTiles.jsx
@@ -1,8 +1,9 @@
+import Image from "next/image";
 import React from "react";
 
 const getTiles = async () => {
-  // const res = await fetch("/data.json");
-  // return await res.json();
+  const res = await fetch("https://tiles-gallery-iota.vercel.app/data.json");
+  return await res.json();
 };
 
 const FeaturesTiles = async () => {
@@ -12,6 +13,22 @@ const FeaturesTiles = async () => {
   return (
     <div className="container mx-auto my-10">
       <h1 className="text-3xl font-bold text-center">Featured Tiles</h1>
+      <div className="grid grid-cols-3 gap-5 mt-3.5">
+        {tiles.map((t) => (
+          <div key={t.id} className="card bg-base-100  shadow-sm">
+            <figure>
+              <Image src={t.image} alt={t.title} width={300} height={300} />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{t.tile}</h2>
+              <p>{t.description}</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">View Details</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
