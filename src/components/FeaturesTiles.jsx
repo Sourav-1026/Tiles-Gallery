a09fb738cbd3ffef6db8e3529460e3469272f12f
Sourav-1026 <faruqueomar740@gmail.com>
2026-05-01 16:04:50 +0600

    vercel json file added

--- src/components/FeaturesTiles.jsx
+++ src/components/FeaturesTiles.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
-const FeaturesTiles = () => {
+const getTiles = async () => {
+  // const res = await fetch("/data.json");
+  // return await res.json();
+};
+
+const FeaturesTiles = async () => {
+  const tiles = await getTiles();
+  console.log(tiles);
+
   return (
     <div className="container mx-auto my-10">
       <h1 className="text-3xl font-bold text-center">Featured Tiles</h1>
