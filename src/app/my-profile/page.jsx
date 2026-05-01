22b60715ecbc5bebc8b91d79b9862fe17a4f899b
Sourav-1026 <faruqueomar740@gmail.com>
2026-05-02 02:18:51 +0600

    my profile section done with update functionality

--- src/app/my-profile/page.jsx
+++ src/app/my-profile/page.jsx
@@ -1,7 +1,27 @@
+"use client";
+
+import { UpdateProfileModal } from "@/components/UpdateProfileModal";
+import { authClient } from "@/lib/auth-client";
+import { Avatar, AvatarFallback, Card } from "@heroui/react";
+
 const MyProfilePage = () => {
+  const userData = authClient.useSession();
+  // console.log(userData);
+  const user = userData.data?.user;
+  console.log(user);
+
   return (
     <div>
-      <h1>This is my profile page</h1>
+      <Card className="max-w-96 mx-auto flex flex-col items-center border border-gray-300 shadow-md my-10">
+        <Avatar className="h-20 w-20">
+          <Avatar.Image alt={user.name} src={user?.image} referrerPolicy="no-referrer" />
+          <AvatarFallback>{user?.name.charAt(0)}</AvatarFallback>
+        </Avatar>
+
+        <h2 className="text-xl font-bold">{user?.name}</h2>
+        <p className="text-muted">{user?.email}</p>
+        <UpdateProfileModal />
+      </Card>
     </div>
   );
 };
